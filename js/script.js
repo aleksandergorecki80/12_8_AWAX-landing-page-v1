0683c9a4652e0bc02499cb7a1b6f17ebd28dd89e
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-07-09 21:47:18 +0100

    Ready but neds code formating

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
 
 $(function(){
 		var $menu = $('nav');
-		$('#menuIcon').click(function(){
+        var $menuIcon = $('#menuIcon');
+        var $menuIconDiv = $('.menu-icon');
+        console.log($menuIconDiv.css('display'));
+        //$menuIconDiv.css('display', 'none');
+		$menuIcon.click(function(){
 			
 			if ($menu.css('display') == "block"){
 
 				$menu.css('display', 'none');
+                $menu.attr('data-state', 'off' );
 				
 			} else{
-				$menu.css('display', 'block' );			
+                $menu.css('display', 'block' );         
+				$menu.attr('data-state', 'on' );
 			}
 		});
 
+
+$(window).resize(function(){
+    var $width = $(window).width();
+    if ($width < 767){
+        $menuIconDiv.css('display', 'block');
+        
+        console.log($menu.attr('data-state'));
+
+        if ($menu.attr('data-state') == 'off'){
+            $menu.css('display', 'none');
+        } else {
+            $menu.css('display', 'block');
+        }
+        
+        
+        $('header .container').css('display', 'block');
+    } else {
+        $menu.css('display', 'block');
+        $menuIconDiv.css('display', 'none');
+        $('header .container').css('display', 'flex');
+    }
+});
+     /*
     $(window).resize(function(){
-        console.log('resiz');
-        if ($(window).width() > 768){
+        var $width = $(window).width();
+        if ($width > 767){
         	$menu.css('display', 'block' );
+            console.log('szer = ' + $width);
         } else {
             $menu.css('display', 'none' );
         }
     });
-
+*/
     var $projectsMenu = $('#menuProjects');
     var $projectsMenuIcon = $('#projectsMenuIcon');
     var $arrow = $('.fa-angle-down');
