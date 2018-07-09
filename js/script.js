396e9448374b20095017ad0a0ff355bd72d27135
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-07-09 23:30:36 +0100

    Fix a resize function

--- js/script.js
+++ js/script.js
@@ -3,12 +3,9 @@ $(function(){
 		var $menu = $('nav');
         var $menuIcon = $('#menuIcon');
         var $menuIconDiv = $('.menu-icon');
-        console.log($menuIconDiv.css('display'));
-        //$menuIconDiv.css('display', 'none');
-		$menuIcon.click(function(){
-			
-			if ($menu.css('display') == "block"){
 
+		$menuIcon.click(function(){                			
+			if ($menu.css('display') == "block"){
 				$menu.css('display', 'none');
                 $menu.attr('data-state', 'off' );
 				
@@ -16,41 +13,22 @@ $(function(){
                 $menu.css('display', 'block' );         
 				$menu.attr('data-state', 'on' );
 			}
+            console.log($menu.attr('data-state'));
 		});
 
-
 $(window).resize(function(){
-    var $width = $(window).width();
-    if ($width < 767){
-        $menuIconDiv.css('display', 'block');
-        
-        console.log($menu.attr('data-state'));
-
-        if ($menu.attr('data-state') == 'off'){
-            $menu.css('display', 'none');
-        } else {
+    var windowWidth = window.innerWidth;
+    
+if (($menuIcon.css('display') == 'block') && ($menu.attr('data-state') == 'on') && windowWidth<768){
             $menu.css('display', 'block');
+        } 
+        if (windowWidth > 768){
+          $menu.css('display', 'block');  
         }
-        
-        
-        $('header .container').css('display', 'block');
-    } else {
-        $menu.css('display', 'block');
-        $menuIconDiv.css('display', 'none');
-        $('header .container').css('display', 'flex');
-    }
+
 });
-     /*
-    $(window).resize(function(){
-        var $width = $(window).width();
-        if ($width > 767){
-        	$menu.css('display', 'block' );
-            console.log('szer = ' + $width);
-        } else {
-            $menu.css('display', 'none' );
-        }
-    });
-*/
+
+
     var $projectsMenu = $('#menuProjects');
     var $projectsMenuIcon = $('#projectsMenuIcon');
     var $arrow = $('.fa-angle-down');
@@ -84,3 +62,7 @@ $('.go-up').on("click",function(){
 });
 
 });
+
+
+
+
