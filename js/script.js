22873fa6bb0667d1c1626c230b52f96331503567
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-07-10 08:00:04 +0100

    Formating js code

--- js/script.js
+++ js/script.js
@@ -1,69 +1,57 @@
+$(function() {
+  var $menu = $("nav");
+  var $menuIcon = $("#menuIcon");
+  var $menuIconDiv = $(".menu-icon");
 
-$(function(){
-		var $menu = $('nav');
-        var $menuIcon = $('#menuIcon');
-        var $menuIconDiv = $('.menu-icon');
+  $menuIcon.click(function() {
+    if ($menu.css("display") == "block") {
+      $menu.css("display", "none");
+      $menu.attr("data-state", "off");
+    } else {
+      $menu.css("display", "block");
+      $menu.attr("data-state", "on");
+    }
+  });
 
-		$menuIcon.click(function(){                			
-			if ($menu.css('display') == "block"){
-				$menu.css('display', 'none');
-                $menu.attr('data-state', 'off' );
-				
-			} else{
-                $menu.css('display', 'block' );         
-				$menu.attr('data-state', 'on' );
-			}
-            console.log($menu.attr('data-state'));
-		});
-
-$(window).resize(function(){
+  $(window).resize(function() {
     var windowWidth = window.innerWidth;
-    
-if (($menuIcon.css('display') == 'block') && ($menu.attr('data-state') == 'on') && windowWidth<768){
-            $menu.css('display', 'block');
-        } 
-        if (windowWidth > 768){
-          $menu.css('display', 'block');  
-        }
 
-});
-
-
-    var $projectsMenu = $('#menuProjects');
-    var $projectsMenuIcon = $('#projectsMenuIcon');
-    var $arrow = document.getElementById('arrowDown');
-
-    $projectsMenuIcon.click(function(){
-        if ($projectsMenu.css('display') == 'none'){
-            $projectsMenu.css('display', 'block'); 
-           $projectsMenuIcon.css({
-                    '-webkit-transform' : 'rotate(180deg)',
-                    '-moz-transform' : 'rotate(180deg)',  
-                    '-ms-transform' : 'rotate(180deg)',  
-                    '-o-transform' : 'rotate(180deg)',  
-                    'transform' : 'rotate(180deg)',  
-                     });
-       } 
-           else {
-            $projectsMenu.css('display', 'none'); 
-            $projectsMenuIcon.css({
-                    '-webkit-transform' : 'rotate(0deg)',
-                    '-moz-transform' : 'rotate(0deg)',  
-                    '-ms-transform' : 'rotate(0deg)',  
-                    '-o-transform' : 'rotate(0deg)',  
-                    'transform' : 'rotate(0deg)',  
-                    });
-        }
-    });
-
-
-$('.go-up').on("click",function(){
-          $("html, body").animate({ scrollTop: 0 }, 600);
+    if (
+      $menuIcon.css("display") == "block" &&
+      $menu.attr("data-state") == "on" &&
+      windowWidth < 768
+    ) {
+      $menu.css("display", "block");
+    }
+    if (windowWidth > 768) {
+      $menu.css("display", "block");
+    }
+  });
+  var $projectsMenu = $("#menuProjects");
+  var $projectsMenuIcon = $("#projectsMenuIcon");
+  $projectsMenuIcon.click(function() {
+    if ($projectsMenu.css("display") == "none") {
+      $projectsMenu.css("display", "block");
+      $projectsMenuIcon.css({
+        "-webkit-transform": "rotate(180deg)",
+        "-moz-transform": "rotate(180deg)",
+        "-ms-transform": "rotate(180deg)",
+        "-o-transform": "rotate(180deg)",
+        transform: "rotate(180deg)"
+      });
+    } else {
+      $projectsMenu.css("display", "none");
+      $projectsMenuIcon.css({
+        "-webkit-transform": "rotate(0deg)",
+        "-moz-transform": "rotate(0deg)",
+        "-ms-transform": "rotate(0deg)",
+        "-o-transform": "rotate(0deg)",
+        transform: "rotate(0deg)"
+      });
+    }
+  });
+  $(".go-up").on("click", function() {
+    $("html, body").animate({ scrollTop: 0 }, 600);
     return false;
+  });
 });
-
-});
-
-
-
-
